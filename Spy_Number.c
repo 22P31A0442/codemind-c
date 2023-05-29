3c3b794f267aad049d9fc736b76f856a47560b3a
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-29 14:50:43 +0530

    #include<stdio.h> int main() {     int n,d,s=0,f=1;     scanf("%d",&n);     while(n!=0)     {         d=n%10;         s=s+d;         f=f*d;         n=n/10;     }     if(s==f)     printf("Spy Number");     else     printf("Not Spy Number");
    }

---
+++ Spy_Number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,d,s=0,f=1;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        d=n%10;
+        s=s+d;
+        f=f*d;
+        n=n/10;
+    }
+    if(s==f)
+    printf("Spy Number");
+    else
+    printf("Not Spy Number");
+    
+}
